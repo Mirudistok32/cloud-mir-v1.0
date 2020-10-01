cdb37acea4c23f6f0c4ae2f6211bd1f9f45b8e12
Mirudistok <mirudistok32@yandex.ru>
2020-10-01 18:40:55 +0300

    Create folder and file config

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+
+const app = express()
+
+// Подключается к базе данных и запускает сервер
+const start = () => {
+    try {
+        
+        app.listen()
+    } catch (error) {
+        
+    }
+}

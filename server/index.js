f24a1e1d1ef8d0e7da4634012c39b465f42874da
Mirudistok <mirudistok32@yandex.ru>
2020-10-01 20:05:29 +0300

    Add app.use for /registration

--- server/index.js
+++ server/index.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("config");
-
+const authRouter = require("./routes/auth.routes");
 const app = express();
 const PORT = config.get("serverPort");
+
+// Первым параметром указываем url, по которому этот роутер будет обрабатываться,
+// а вторым параметром передаем сам роутер
+app.use("/api/auth", authRouter);
+
 // Подключается к базе данных и запускает сервер
 // Подключение к базе данных - это асинхронный процесс
 const start = async () => {
   try {
-
     // Подключаемся к базе данных
     // Первым параметром принимает url к базе данных
-    await mongoose.connect(config.get("dbUrl"))
+    await mongoose.connect(config.get("dbUrl"));
 
     //Первым параметром на каком порту будет запущен сервер, а вторым параметром принимает функцию, которая запускается сразу после запуска сервера
     app.listen(PORT, () => {

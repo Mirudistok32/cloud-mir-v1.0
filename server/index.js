ac514e328192c6628aed90a21a862b192eb25aa8
Mirudistok <mirudistok32@yandex.ru>
2020-10-01 18:53:14 +0300

    Connect to mongoDB

--- server/index.js
+++ server/index.js
@@ -5,8 +5,14 @@ const config = require("config");
 const app = express();
 const PORT = config.get("serverPort");
 // Подключается к базе данных и запускает сервер
-const start = () => {
+// Подключение к базе данных - это асинхронный процесс
+const start = async () => {
   try {
+
+    // Подключаемся к базе данных
+    // Первым параметром принимает url к базе данных
+    await mongoose.connect(config.get("dbUrl"))
+
     //Первым параметром на каком порту будет запущен сервер, а вторым параметром принимает функцию, которая запускается сразу после запуска сервера
     app.listen(PORT, () => {
       console.log("Server start on port, ", PORT);

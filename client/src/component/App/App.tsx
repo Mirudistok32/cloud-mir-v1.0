34ec2818e9ce424c24537c049dfa32000ec8cd94
Mirudistok <mirudistok32@yandex.ru>
2020-10-01 22:49:51 +0300

    react-router-dom

--- client/src/component/App/App.tsx
+++ client/src/component/App/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import s from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className={s.app}>
+
     </div>
   );
 }

f478df498a89f163db104ee33871e492f16f8b52
Mirudistok <mirudistok32@yandex.ru>
2020-10-01 22:44:36 +0300

    Add type for store, and add middleware thunk

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,10 +1,17 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import thunk from "redux-thunk";
 
-const rootReducre = combineReducers({
+const rootReducers = combineReducers({
 
 })
 
-const store = createStore(rootReducre,)
+type RootReducersType = typeof rootReducers
+export type AppStateType = ReturnType<RootReducersType>
+
+type PropertiesTypes<T> = T extends { [key: string]: infer U } ? U : never
+export type InferActionsTypes<T extends { [key: string]: (...args: any[]) => any }> = ReturnType<PropertiesTypes<T>>
+
+const store = createStore(rootReducers, applyMiddleware(thunk))
 
 
 export default store

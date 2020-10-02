30eb7de3be81fccf18ac1281ddb6ab8a563b4d94
Mirudistok <mirudistok32@yandex.ru>
2020-10-02 20:56:14 +0300

    Add Navlink in Navnar

--- client/src/component/Navbar/Navbar.tsx
+++ client/src/component/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import s from './Navbar.module.scss'
 import logo from '../../assets/images/logo.svg'
+import { NavLink } from 'react-router-dom'
 
 type PropsType = {
 
@@ -14,8 +15,8 @@ export const Navbar: React.FC<PropsType> = React.memo((props) => {
         <div className={s.navbar}>
             <img className={s.navbar__logo} src={logo} alt="" />
             <div className={s.navbar__header}>MERN CLOUD</div>
-            <div className={s.navbar__login}>Войти</div>
-            <div className={s.navbar__registration}>Регистрация</div>
+            <div className={s.navbar__login}><NavLink to={'/login'}>Войти</NavLink></div>
+            <div className={s.navbar__registration}><NavLink to={'/registration'}>Регистрация</NavLink></div>
         </div>
     )
 })

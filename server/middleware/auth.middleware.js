8f77bad58f42251f2d9faeb8fbfc394985153424
Mirudistok <mirudistok32@yandex.ru>
2020-10-02 23:29:46 +0300

    r

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -13,7 +13,7 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(" ")[1];
 
     if (!token) {
-      return res.status(401).json({ message: "Auth error" });
+      return res.status(401).json({ message: "Auth error, no token" });
     }
     // Раскодируем token
     const decoded = jwt.verify(token, config.get("secretKey"));

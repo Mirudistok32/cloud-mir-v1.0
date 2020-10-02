ec93c48e4348d8e504ff5c84af243ca01ff01d93
Mirudistok <mirudistok32@yandex.ru>
2020-10-02 22:38:36 +0300

    Add Navlink Выйти in Navbar component

--- client/src/component/Navbar/Navbar.tsx
+++ client/src/component/Navbar/Navbar.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import s from './Navbar.module.scss'
 import logo from '../../assets/images/logo.svg'
 import { NavLink } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { AppStateType } from '../../redux/store'
 
 type PropsType = {
 
@@ -11,12 +13,15 @@ export const Navbar: React.FC<PropsType> = React.memo((props) => {
 
     const { } = props
 
+    const isAuth = useSelector((state: AppStateType) => state.userReducer.isAuth)
+
     return (
         <div className={s.navbar}>
             <img className={s.navbar__logo} src={logo} alt="" />
             <div className={s.navbar__header}>MERN CLOUD</div>
-            <div className={s.navbar__login}><NavLink to={'/login'}>Войти</NavLink></div>
-            <div className={s.navbar__registration}><NavLink to={'/registration'}>Регистрация</NavLink></div>
+            {!isAuth && <div className={s.navbar__login}><NavLink to={'/login'}>Войти</NavLink></div>}
+            {!isAuth && <div className={s.navbar__registration}><NavLink to={'/registration'}>Регистрация</NavLink></div>}
+            {isAuth && <div className={s.navbar__login}><NavLink to={'/registration'}>Выйти</NavLink></div>}
         </div>
     )
 })

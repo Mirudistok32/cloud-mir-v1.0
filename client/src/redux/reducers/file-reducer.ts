43bba377bce3bba9f8dff465b718a7fe906ebb87
Mirudistok <mirudistok32@yandex.ru>
2020-10-03 16:17:48 +0300

    Create actionsFileReducer setFiles

--- client/src/redux/reducers/file-reducer.ts
+++ client/src/redux/reducers/file-reducer.ts
@@ -1,19 +1,39 @@
 import { InferActionsTypes } from './../store';
 
+type FileChildsType = {
+    type: string,
+    ref: string
+}
+
+type FileType = {
+    name: string
+    type: string
+    accessLink: string
+    size: { type: Number, default: 0 },
+    path: string
+    user: string
+    parent: string
+    childs: Array<FileChildsType>
+}
+
 
 type InitialStateType = {
-
+    files: Array<FileType> | null
+    currentDir: string | null
 }
 
 const initialState: InitialStateType = {
-
+    files: [],
+    currentDir: null
 }
 
 type ActionsTypes = InferActionsTypes<typeof actionsFileReducer>
 
 export const fileReducer = (state = initialState, action: ActionsTypes): InitialStateType => {
     switch (action.type) {
-
+        case "MIRUDISTOK/FILE/SET_FILES": {
+            return { ...state, ...action.payload }
+        }
         default:
             return state
     }
@@ -21,5 +41,5 @@ export const fileReducer = (state = initialState, action: ActionsTypes): Initial
 
 
 export const actionsFileReducer = {
-
+    setFiles: (file: FileType) => ({ type: "MIRUDISTOK/FILE/SET_FILES", payload: file } as const)
 }

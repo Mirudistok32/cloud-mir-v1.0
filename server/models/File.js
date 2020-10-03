f472c0a0d903b83aa761febc7d0690ea192142ea
Mirudistok <mirudistok32@yandex.ru>
2020-10-03 13:12:11 +0300

    SERVER: Create File model

---
+++ server/models/File.js
@@ -0,0 +1,14 @@
+const { model, Schema, ObjectId } = require("mongoose");
+
+const File = new Schema({
+  name: { type: String, required: true },
+  type: { type: String, required: true },
+  accessLink: { type: String },
+  size: { type: Number, default: 0 },
+  path: { type: String, default: "" },
+  user: { type: ObjectId, ref: "User" }, // Ссылается на пользователя, который добавил файл
+  parent: { type: ObjectId, ref: "File" }, // Ссылается на файл, в папку в которой он находится
+  childs: [{ type: ObjectId, ref: "File" }], // Ссылается на все файлы, которые будут лежать внутри папки
+});
+
+module.exports = model("File", File);

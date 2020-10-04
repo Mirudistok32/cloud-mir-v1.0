8b4504a979a9590877dc2122ed8dd3bf9d02c10c
Mirudistok <mirudistok32@yandex.ru>
2020-10-04 18:35:59 +0300

    popup

---
+++ client/src/component/Disk/Popup/Popup.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import s from './Popup.module.scss'
+
+const Popup = () => {
+
+
+    return (
+        <div className={s.popup}>
+
+        </div>
+    )
+}
+
+export default Popup
+
+
